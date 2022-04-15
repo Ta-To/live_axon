8494699b1f5c35e481dfb8770e7fdc82036eabca
Ta-To <61908291+Ta-To@users.noreply.github.com>
2022-04-15 16:28:54 +0900

    FEAT 手書き（キャンバス）数字の推測結果出力に対応

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,8 +26,13 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+import Mnist from "./mnist"
+const Hooks = {
+  Mnist: Mnist
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
